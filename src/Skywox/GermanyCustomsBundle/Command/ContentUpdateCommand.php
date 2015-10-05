4ef6718a43a71b6dd93b0dc433b0901685bfe394
Boris Shevchenko <info@bshevchenko.ru>
2015-10-05 13:24:44 +0300

    refactored: execute method splitted to multiple methods

--- src/Skywox/GermanyCustomsBundle/Command/ContentUpdateCommand.php
+++ src/Skywox/GermanyCustomsBundle/Command/ContentUpdateCommand.php
@@ -3,6 +3,7 @@
 namespace Skywox\GermanyCustomsBundle\Command;
 
 use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\EntityManager;
 use Skywox\GermanyCustomsBundle\Type\EDIDateTime;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -18,6 +19,12 @@ use Symfony\Component\Yaml\Parser;
  */
 class ContentUpdateCommand extends ContainerAwareCommand
 {
+    /** @var Parser */
+    private $yamlParser;
+
+    /** @var EntityManager */
+    private $entityManager;
+
     protected function configure()
     {
         $this
@@ -31,11 +38,13 @@ class ContentUpdateCommand extends ContainerAwareCommand
      * @param OutputInterface $output
      * @return int|null|void
      *
-     * TODO Split into multiple methods
      * TODO Optimize command. Current realization is too slow (1 row per second after 10 minutes of processing)
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->yamlParser = new Parser();
+        $this->entityManager = $this->getContainer()->get('doctrine')->getManager();
+
         $edifactFilename = $input->getArgument('edifactFilename');
 
         if (!file_exists($edifactFilename)) {
@@ -45,21 +54,22 @@ class ContentUpdateCommand extends ContainerAwareCommand
 
         $fp = fopen($edifactFilename, 'r');
 
+        $skip = 21000; // temporarily variable for testing to skip processed rows
         $counter = 0;
-        $skip = 13200;
-        $entityManager = $this->getContainer()->get('doctrine')->getManager();
-        $yamlParser = new Parser();
         $time = 0;
         while (!feof($fp)) {
             $counter++;
 
             $msg = fgets($fp);
 
+            // skip processed rows
             if ($counter <= $skip) {
                 continue;
             }
 
-            if (substr($msg, 0, 3) == 'UNB' || substr($msg, 0, 3) == 'UNH') {
+            // skip headers strings
+            $msgHeader = substr($msg, 0, 3);
+            if (in_array($msgHeader, ['UNB', 'UNH', 'UNT', 'UNZ'])) {
                 continue;
             }
 
@@ -70,113 +80,145 @@ class ContentUpdateCommand extends ContainerAwareCommand
             $operation = substr($msg, 15, 1); // i - insert, u - update, d - delete
             $data = substr($msg, 16);
 
+            // TODO This is only for testing to catch unexpected values, delete in production mode
             if (strlen($id) < 5 || strlen($number) < 4 || strlen($table) < 6 || !in_array($table[0], ['T', 'N']) || $operation != 'i') {
                 var_dump($id, $number, $table, $operation, $data);
                 continue;
                 //die;
             }
 
+            // Count speed of command processing
             if ($counter % 100 == 0) {
                 echo $counter . ' ' . round((100 / (microtime(true) - $time))) . ' rows per second' . PHP_EOL;
-
                 $time = microtime(true);
             }
 
-            $tableSchemaFile = __DIR__ . '/../Resources/config/doctrine/' . $table . '.orm.yml';
-//            if (!file_exists($tableSchemaFile)) {
-//                $output->writeln('table schema file "' . $tableSchemaFile . '" not found');
-//                return;
-//            }
-
-            $yaml = $yamlParser->parse(file_get_contents($tableSchemaFile));
-            $tableEntity = array_keys($yaml)[0];
-
-            $fields = array_merge($yaml[$tableEntity]['id'], $yaml[$tableEntity]['fields']);
-
-            switch ($operation) {
-                case 'i': // insert
-                    $entity = new $tableEntity;
-                    break;
-
-                case 'u': // update
-                    $entity = new $tableEntity;
-                    break;
-
-                case 'd': // delete
-                    $entity = new $tableEntity;
-                    break;
-
-                default:
-                    throw new \UnexpectedValueException('undefined operation "' . $operation . '"');
-            }
-
+            // fill out entity with new values
+            list($tableEntity, $fields) = $this->getTableStructure($table);
+            $entity = $this->getEntityByOperation($operation, $tableEntity);
             $start = 0;
             foreach ($fields as $name => $options) {
                 $setterName = 'set' . str_replace('_', '', $name);
-
-                switch ($options['otype']) {
-                    case 'CHAR':
-                        $value = substr($data, $start, $options['length']);
-                        $start += $options['length'];
-                        break;
-
-                    case 'VARCHAR':
-                        $endPos = strpos(substr($data, $start + 1), '"+');
-                        $value = substr($data, $start + 1, $endPos);
-                        $start += strlen($value) + 3;
-                        break;
-
-                    case 'DATE':
-                        // format is DDMMYYYYHH24MISS
-                        $value = EDIDateTime::ediCreateFromFormat('dmYHis', substr($data, $start, 14));
-                        $start += 14;
-                        break;
-
-                    case 'NUMBER':
-                        if ($options['type'] == 'decimal') {
-                            $value = (float)str_replace(',', '.', substr($data, $start, $options['precision'] + 2)); // 1 symbol uses for number sign +/-, 1 symbol for comma
-                            $start += $options['precision'] + 2;
-                        } else {
-                            $value = (int)substr($data, $start, $options['length'] + 1); // 1 symbol uses for number sign +/-
-                            $start += $options['length'] + 2; // +1 symbol for comma
-                        }
-
-                        break;
-
-                    case 'LONG':
-                        $length = (int)substr($data, $start, 9);
-                        $start += 9;
-                        $value = substr($data, $start, $length);
-                        while (strlen($value) < $length) {
-                            $value .= fgets($fp);
-                        }
-                        $start += $length;
-                        break;
-
-                    case 'CLOB':
-                        $length = (int)substr($data, $start, 9);
-                        $start += 9;
-                        $value = '';
-                        while (strlen($value) < $length) {
-                            $value .= fgets($fp);
-                        }
-                        break;
-
-                    default:
-                        throw new \UnexpectedValueException('undefined field type "' . $options['type'] . '"');
-                }
-
+                $value = $this->parseColumnValue($options, $data, $start, $fp);
                 $entity->$setterName($value);
             }
 
+            // insert, update or delete row in MySQL database
             try {
-                $entityManager->merge($entity);
-                $entityManager->flush();
+                $this->entityManager->merge($entity);
+                $this->entityManager->flush();
             } catch (DBALException $e) {
                 continue;
             }
         }
 
         fclose($fp);
+    }
+
+    /**
+     * @param $table
+     * @return array
+     */
+    private function getTableStructure($table)
+    {
+        $tableSchemaFile = __DIR__ . '/../Resources/config/doctrine/' . $table . '.orm.yml';
+        if (!file_exists($tableSchemaFile)) {
+            throw new \UnexpectedValueException('table schema file "' . $tableSchemaFile . '" not found');
+        }
+
+        $yaml = $this->yamlParser->parse(file_get_contents($tableSchemaFile));
+        $tableEntity = array_keys($yaml)[0];
+
+        $fields = array_merge($yaml[$tableEntity]['id'], $yaml[$tableEntity]['fields']);
+        return [$tableEntity, $fields];
+    }
+
+    /**
+     * @param $operation
+     * @param $tableEntity
+     * @return mixed
+     */
+    private function getEntityByOperation($operation, $tableEntity)
+    {
+        switch ($operation) {
+            case 'i': // insert
+                $entity = new $tableEntity;
+                break;
+
+            case 'u': // update
+                $entity = new $tableEntity;
+                break;
+
+            case 'd': // delete
+                $entity = new $tableEntity;
+                break;
+
+            default:
+                throw new \UnexpectedValueException('undefined operation "' . $operation . '"');
+        }
+        return $entity;
+    }
+
+    /**
+     * @param $options
+     * @param $data
+     * @param $start
+     * @param $fp
+     * @return \DateTime|float|int|string
+     */
+    private function parseColumnValue($options, $data, &$start, $fp)
+    {
+        switch ($options['otype']) {
+            case 'CHAR':
+                $value = substr($data, $start, $options['length']);
+                $start += $options['length'];
+                break;
+
+            case 'VARCHAR':
+                $endPos = strpos(substr($data, $start + 1), '"+');
+                $value = substr($data, $start + 1, $endPos);
+                $start += strlen($value) + 3;
+                break;
+
+            case 'DATE':
+                // format is DDMMYYYYHH24MISS
+                $value = EDIDateTime::ediCreateFromFormat('dmYHis', substr($data, $start, 14));
+                $start += 14;
+                break;
+
+            case 'NUMBER':
+                if ($options['type'] == 'decimal') {
+                    $value = (float)str_replace(',', '.', substr($data, $start, $options['precision'] + 2)); // 1 symbol uses for number sign +/-, 1 symbol for comma
+                    $start += $options['precision'] + 2;
+                } else {
+                    $value = (int)substr($data, $start, $options['length'] + 1); // 1 symbol uses for number sign +/-
+                    $start += $options['length'] + 2; // +1 symbol for comma
+                }
+
+                break;
+
+            case 'LONG':
+                $length = (int)substr($data, $start, 9);
+                $start += 9;
+                $value = substr($data, $start, $length);
+                while (strlen($value) < $length) {
+                    $value .= fgets($fp);
+                }
+                $start += $length;
+                break;
+
+            case 'CLOB':
+                $length = (int)substr($data, $start, 9);
+                $start += 9;
+                $value = '';
+                while (strlen($value) < $length) {
+                    $value .= fgets($fp);
+                }
+                break;
+
+            default:
+                throw new \UnexpectedValueException('undefined field type "' . $options['type'] . '"');
+        }
+        return $value;
     }
 }
